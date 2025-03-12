312aa70361f9221f313b6814547849489a3c5d9e
ronito19 <ronnie_1019@hotmail.com>
2025-03-13 00:14:22 +0100

    Borrando codigo inicial del paginado

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -13,31 +13,15 @@ import { UsuarioCardComponent } from "../../components/usuario-card/usuario-card
 export class UsersComponent {
   arrUsuariosPromises: IUsuario[] = [];
   usuariosServices = inject(UsuariosService);
-  linkPrev: string = "";
-  linkNext: string = "";
 
   async ngOnInit() {
     try {
       let response: IResponse = await this.usuariosServices.getAllPromise()
-      this.linkNext = response.links.next;
-      this.linkPrev = response.links.previous;
+      console.log(this.arrUsuariosPromises)
       this.arrUsuariosPromises = response.results
       
     }catch (error) {
       console.log(error)
     }
-  }
-
-
-  async goToNext() {
-    let response: IResponse = await this.usuariosServices.getAllPromise(this.linkNext);
-    this.linkNext = response.links.next;
-    this.linkPrev = response.links.previous;
-    this.arrUsuariosPromises = response.results;
-  }
-
-
-  goToPrev() {
-    
   }
 }
--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -11,8 +11,8 @@ export class UsuariosService {
   private httpClient = inject(HttpClient)
   private urlExterna: string = "https://peticiones.online/api/users"
 
-  getAllPromise(url= "https://peticiones.online/api/users?limit=5"): Promise<IResponse> {
-    return lastValueFrom(this.httpClient.get<IResponse>(url))
+  getAllPromise(): Promise<IResponse> {
+    return lastValueFrom(this.httpClient.get<IResponse>(this.urlExterna))
   }
 
 
