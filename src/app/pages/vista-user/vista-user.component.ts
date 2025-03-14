7bc42db26f40bc25d688d8b5a5292a18243cf142
ronito19 <ronnie_1019@hotmail.com>
2025-03-14 12:39:30 +0100

    Maquetacion de la vista-usuario terminada

--- src/app/pages/vista-user/vista-user.component.ts
+++ src/app/pages/vista-user/vista-user.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject, Input } from '@angular/core';
 import { IUsuario } from '../../interfaces/iusuario.interface';
 import { UsuariosService } from '../../services/usuarios.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-vista-user',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './vista-user.component.html',
   styleUrl: './vista-user.component.css'
 })
